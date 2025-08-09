13b4cc8324bb9e3b3100b765fb80527f367385a0
afeeeff <safeef16@gmail.com>
2025-08-10 00:05:17 +0530

    cors update3

--- review-app-backend/server.js
+++ review-app-backend/server.js
@@ -27,8 +27,9 @@ const allowedOrigins = [
       'http://localhost:5174',
       'http://localhost:5175',
       'http://localhost:5176',
-      'demouser.instantreviews.in',
-      'demoreviewapp.vercel.app'
+      
+      'https://demoreviewapp.vercel.app',
+      'https://demouser.instantreviews.in'
 
        // Your local Vite development server (if using Vite default)
       
